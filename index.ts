0935631a4922cb9b48b8d784a6efd800e9c1160f
Travis Arnold <ftntravis@gmail.com>
2022-10-12 00:10:52 -0700

    add options

--- index.ts
+++ index.ts
@@ -1,38 +1,78 @@
 import { useEffect, useLayoutEffect, useRef } from "react"
 import sync, { cancelSync } from "framesync"
 
+export { flushSync, getFrameData } from "framesync"
+export { cancelSync, sync }
+
 const isServer = typeof window === "undefined"
 const useIsomorphicLayoutEffect = isServer ? useEffect : useLayoutEffect
 
 export function useFrame(
-  callback: (
-    frame: Parameters<typeof sync.render>[0],
-    cancel: () => void,
-  ) => void,
-  enabled: boolean = true,
+  callback: (event: {
+    /** Cancel the current scheduled frame. */
+    cancel: () => void
+
+    /** The time elapsed since mounting or re-enabling. */
+    elapsed: number
+
+    /** Data about the current frame. */
+    frame: {
+      delta: number
+      timestamp: number
+    }
+  }) => void,
+  {
+    enabled = true,
+    once = true,
+    type = "update",
+  }: {
+    /** Whether or not the frame callback should run. */
+    enabled: boolean
+
+    /** Only runs the scheduled frame one time. */
+    once: boolean
+
+    /** The priority of execution across a frame. */
+    type:
+      | "postRender"
+      | "postUpdate"
+      | "preRender"
+      | "read"
+      | "render"
+      | "update"
+  },
 ) {
-  const processRef = useRef<ReturnType<typeof sync.render>>()
   const callbackRef = useRef<any>()
 
   callbackRef.current = callback
 
   useIsomorphicLayoutEffect(() => {
-    const cancel = () => process && cancelSync.render(process)
-    let process = processRef.current
+    const cancel = () => {
+      process && cancelSync[type](process)
+      initialTimestamp = null
+    }
+    let initialTimestamp: number | null = null
+    let process: any
 
     cancel()
 
     if (enabled) {
-      process = sync.render((frame) => {
-        callbackRef.current(frame, cancel)
-      }, true)
-    }
+      process = sync[type](({ delta, timestamp }) => {
+        if (!initialTimestamp) {
+          initialTimestamp = timestamp
+        }
 
-    processRef.current = process
+        callbackRef.current({
+          cancel,
+          elapsed: timestamp - initialTimestamp,
+          frame: { delta, timestamp },
+        })
+      }, !once)
+    }
 
     return () => {
       if (process) {
-        cancelSync.render(process)
+        cancelSync[type](process)
       }
     }
   }, [enabled])
